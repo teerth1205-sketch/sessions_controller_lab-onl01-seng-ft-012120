8921f7cb2a24f497d53b05a9c7e0ad1b97b9c3b5
Teerth Patel <teerth1205@gmail.com>
2020-04-07 23:21:45 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   
   def create
     if params[:name].nil?
-      redirect_to ''
+      redirect_to '/login'
+    else
     session[:name] = params[:name]
         redirect_to '/'
   end 
